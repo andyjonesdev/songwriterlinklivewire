43dff1dcf0b06b22ab7070448a92a47f9bc35ccb
andyarcimedia <61969705+andyarcimedia@users.noreply.github.com>
2026-01-27 21:42:51 +0000

    Added gtag

--- routes/web.php
+++ routes/web.php
@@ -47,7 +47,7 @@ Route::middleware(['auth'])->group(function () {
 });
 
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth'])->group(function () {
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     // Route::get('/dashboard/buyer', [BuyerDashboardController::class, 'index'])->name('buyerdashboard');
